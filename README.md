d2a2545caa01403cf1224b173b976379525847b2
Umaid Abbasi <umaidabbasi@gmail.com>
2022-08-15 12:04:27 -0400

    Match on the home team id and then the away team id to get the separate ranks
    Refactor to allow easier implementation for unit tests

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ACME-Sports
 
-This project is a simple command line program written in `Python 3.X` that fetches and parses data from 2 API endpoints,
+This project is a simple command line program written using Python, that fetches and parses data from 2 API endpoints,
 merges the data and returns a json formatted string.
+
+Please use `python 3.6` or greater.
 
 
 ## Dependency installation
@@ -52,8 +54,8 @@ Example output:
         "away_team_id": "42",
         "away_nick_name": "Texans",
         "away_city": "Houston",
-        "away_rank": "3",
-        "away_rank_points": "19.28",
+        "away_rank": "26",
+        "away_rank_points": "-11.00",
         "home_team_id": "63",
         "home_nick_name": "Chiefs",
         "home_city": "Kansas City",
@@ -67,8 +69,8 @@ Example output:
         "away_team_id": "52",
         "away_nick_name": "Seahawks",
         "away_city": "Seattle",
-        "away_rank": "7",
-        "away_rank_points": "10.11",
+        "away_rank": "14",
+        "away_rank_points": "3.39",
         "home_team_id": "39",
         "home_nick_name": "Packers",
         "home_city": "Green Bay",
@@ -82,8 +84,8 @@ Example output:
         "away_team_id": "62",
         "away_nick_name": "Titans",
         "away_city": "Tennessee",
-        "away_rank": "3",
-        "away_rank_points": "19.28",
+        "away_rank": "12",
+        "away_rank_points": "5.12",
         "home_team_id": "63",
         "home_nick_name": "Chiefs",
         "home_city": "Kansas City",
@@ -97,8 +99,8 @@ Example output:
         "away_team_id": "39",
         "away_nick_name": "Packers",
         "away_city": "Green Bay",
-        "away_rank": "6",
-        "away_rank_points": "10.34",
+        "away_rank": "7",
+        "away_rank_points": "10.11",
         "home_team_id": "58",
         "home_nick_name": "49ers",
         "home_city": "San Francisco",
@@ -106,7 +108,6 @@ Example output:
         "home_rank_points": "10.34"
     }
 ]
-
 
 ```
 
@@ -125,45 +126,10 @@ python -m 'pytest' test_main.py
 - logging
 - json 
 - datetime
+- sys
 
 
 ## Improvements 
 
-- More tests can be written 
+- More tests can be written, specially we can use fixtures to load the API responses. This would allow us too test the functions that parse the API responses.
 - In `parse_team_rankings` and `parse_score_board` methods I am using a nested for loop. This will not scale well. The time complexity is 0(n^2).
-
-
-## Assumptions 
-
-It seems that either the example response at the end of the `Python Development Challenge ` document is wrong or the table on page 2 is wrong. 
-
-My explanation below:
-
-If we take a look at the first element in the example response: 
-
-```bash
-{
-"event_id": "1233827",
-"event_date": "12-01-2020",
-"event_time": "15:05",
-"away_team_id": "42",
-"away_nick_name": "Texans",
-"away_city": "Houston",
-"away_rank": "21", <----- Here
-"away_rank_points": "-6.00", <----- Here
-"home_team_id": "63",
-"home_nick_name": "Chiefs",
-"home_city": "Kansas City",
-"home_rank": "2", <----- Here
-"home_rank_points": "21.20" <----- Here
-}
-
-```
-
-We can see that `away_rank` is `21` and `home_rank` is `2`. 
-
-However, in the table on page 2 (see below), `away_rank` and `home_rank` are being sourced from the same source label `rank` and from the same source endpoint `/scoreboard`. There is no formatting being asked, so I am just wondering how in the example response these 2 keys differ. 
-
-The same thing can be seen for `home_rank_points` and `away_rank_points`
-
-![img.png](images/img.png)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 import requests
 import logging
 import json
@@ -37,12 +38,14 @@ class ApiHandler:
             exit(1)
         return team_ranking_response
 
-    def get_score_board(self) -> requests.Response:
+    def get_score_board(self, start_date, end_date) -> requests.Response:
         """
         Makes a GET request to the scoreboard endpoint
+        :param start_date: The start date to calculate scoreboard from
+        :param end_date: The end date to calculate scoreboard from
         :return: request.Response
         """
-        url = f'{self.api_url}/scoreboard/{LEAGUE}/{args.START_DATE}/{args.END_DATE}?api_key={self.api_key}'
+        url = f'{self.api_url}/scoreboard/{LEAGUE}/{start_date}/{end_date}?api_key={self.api_key}'
         scoreboard_response = requests.get(url)
         # Use the API to handle bad inputs, such as reverse start and end dates or start and end dates greater than 7
         # days
@@ -53,13 +56,15 @@ class ApiHandler:
         return scoreboard_response
 
 
-def parse_score_board(api_helper: ApiHandler) -> List[dict]:
+def parse_score_board(start_date, end_date, api_helper: ApiHandler) -> List[dict]:
     """
     Parses the data from get_score_board method.
+    :param start_date: The start date to calculate scoreboard from
+    :param end_date: The end date to calculate scoreboard from
     :param api_helper: API helper object.
     :return:
     """
-    score_rankings_raw = api_helper.get_score_board().json()
+    score_rankings_raw = api_helper.get_score_board(start_date=start_date, end_date=end_date).json()
     list_of_score_rankings = []
     for outer_key in score_rankings_raw['results'].keys():
         if score_rankings_raw['results'][outer_key]:
@@ -75,53 +80,43 @@ def parse_score_board(api_helper: ApiHandler) -> List[dict]:
                     'away_team_id']
                 new_score_rankings['away_nick_name'] = score_rankings_raw['results'][outer_key]['data'][inner_key][
                     'away_nick_name']
-                new_score_rankings['away_city'] = score_rankings_raw['results'][outer_key]['data'][inner_key]['away_city']
+                new_score_rankings['away_city'] = \
+                    score_rankings_raw['results'][outer_key]['data'][inner_key]['away_city']
                 new_score_rankings['away_rank'] = None
                 new_score_rankings['away_rank_points'] = None
                 new_score_rankings['home_team_id'] = score_rankings_raw['results'][outer_key]['data'][inner_key][
                     'home_team_id']
                 new_score_rankings['home_nick_name'] = score_rankings_raw['results'][outer_key]['data'][inner_key][
                     'home_nick_name']
-                new_score_rankings['home_city'] = score_rankings_raw['results'][outer_key]['data'][inner_key]['home_city']
+                new_score_rankings['home_city'] = \
+                    score_rankings_raw['results'][outer_key]['data'][inner_key]['home_city']
                 new_score_rankings['home_rank'] = None
                 new_score_rankings['home_rank_points'] = None
                 list_of_score_rankings.append(new_score_rankings)
     return list_of_score_rankings
 
 
-def parse_team_rankings(api_helper: ApiHandler) -> List[dict]:
+def parse_team_rankings(score_board_response, api_helper) -> List[dict]:
     """
     Parses data from team_ranking method, updates the data structure returned from the parse_score_board method
-    :param api_helper:
-    :return:
+    :param score_board_response: Response from scoreboard parser
+    :param api_helper: API hadler method
+    :return: List of dictionaries
     """
-    formatted_response = parse_score_board(api_helper)
 
     team_rankings_raw = api_helper.get_team_rankings().json()
     for team_rank in team_rankings_raw['results']['data']:
-        for response in formatted_response:
-            # Team_id from /team_rankings end point matches the home_team_id from the /score_board endpoint.
-            # using these IDs to merge data between the 2 endpoints.
+        for response in score_board_response:
+            # Team_id from the /team_rankings end point matches the home_team_id and the away_team_id from the
+            # /score_board endpoint. Using these IDs to merge data between the 2 endpoints.
+            # Using f strings to round adjusted_points to 2 decimal places
             if team_rank['team_id'] == response['home_team_id']:
-                response['away_rank'] = team_rank['rank']
-                response['away_rank_points'] = str(round(float(team_rank['adjusted_points']), 2))
                 response['home_rank'] = team_rank['rank']
-                response['home_rank_points'] = response['away_rank_points']
-    return formatted_response
-
-
-def entry_point(api_key: str):
-    """
-    Entry point for script. Prints response in JSON
-    :param api_key: API key, sourced from the command line
-    :return: None
-    """
-    api_helper = ApiHandler(api_key=api_key)
-    formatted_output = parse_team_rankings(api_helper)
-    if formatted_output:
-        print(json.dumps(formatted_output, indent=4))
-    else:
-        print('Response was empty, please try another start and end time')
+                response['home_rank_points'] = f"{float(team_rank['adjusted_points']):.2f}"
+            if team_rank['team_id'] == response['away_team_id']:
+                response['away_rank'] = team_rank['rank']
+                response['away_rank_points'] = f"{float(team_rank['adjusted_points']):.2f}"
+    return score_board_response
 
 
 def parse_time(date_time_str: str) -> str:
@@ -144,7 +139,15 @@ def parse_date(date_time_str: str) -> str:
     return date_time_obj.strftime('%d-%m-%Y')
 
 
-if __name__ == "__main__":
+def entry_point(argv=None):
+    """
+    Entry point for script. Creates an arg_parser.
+    Prints response in JSON, If response from parse_team_rankings is empty, informs user that
+    the response was empty.
+    :param argv: Used too pass in arguments when program is not called from command line. Such as in tests.
+    :return: None
+    """
+    # create arg parser
     parser = argparse.ArgumentParser(prog='main', description='List NFL data gathered from remote API')
 
     parser.add_argument('-api_key', required=True, action='store', dest='API_KEY',
@@ -153,8 +156,20 @@ if __name__ == "__main__":
                         help='Start date used to narrow down the scoreboard ')
     parser.add_argument('-end_date', required=True, action='store', dest='END_DATE',
                         help='End date used to narrow down the scoreboard')
-
     parser.add_argument('-v', '-version', action='version', version='1.0.0')
-    args = parser.parse_args()
-    entry_point(api_key=args.API_KEY)
+    args = parser.parse_args(argv)
 
+    # create api handler object
+    api_helper = ApiHandler(api_key=args.API_KEY)
+    # call scoreboard parser method with start and end dates taken from command line
+    score_board_response = parse_score_board(start_date=args.START_DATE, end_date=args.END_DATE, api_helper=api_helper)
+    # call team rankings, takes data returned by the parse_score_board method
+    formatted_output = parse_team_rankings(score_board_response=score_board_response, api_helper=api_helper)
+    if formatted_output:
+        print(json.dumps(formatted_output, indent=4))
+    else:
+        print('Response was empty, please try another start and end time')
+
+
+if __name__ == "__main__":
+    sys.exit(entry_point())
